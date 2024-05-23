67ae55ecb5b89d568a1c795d996d4aa0b090c1ae
em22ad <em22ad@gmail.com>
2024-05-23 00:09:31 -0500

    Update embedded_c_algo_1.1.c

--- embedded_c_algo_1.1.c
+++ embedded_c_algo_1.1.c
@@ -1,7 +1,6 @@
 // read_preprocess.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
